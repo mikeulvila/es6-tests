51ac8b04efc218f73644b2d3c3c78acb68781fea
Mike Ulvila <mikeulvila@me.com>
2016-01-25 15:28:23 -0600

    finished template string tests

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -3,9 +3,36 @@ var expect = require('chai').expect;
 describe('template strings', function () {
   it('should be string', function () {
     var string = `abc`;
-    expect(string).to.be.a('number');
-    
+
+    expect(string).to.be.a('string');
   });
 
+  it('can evaluate variables', function () {
+    var bar = 'baz';
+    var string = `foo${bar}`;
 
+    expect(string).to.equal('foobaz');
+  });
+
+  it('can execute functions', function () {
+    var string = `The date is: ${new Date()}`;
+    var date = new Date();
+    
+    expect(string).to.equal('The date is: ' + date);
+  });
+
+  it('can evaluate expressions', function () {
+    var x = 1;
+    var y = 2;
+    var sum = `The total is ${x + y}`;
+    expect(sum).to.equal('The total is ' + (x+y));
+  });
+
+  it('can handle multiline strings', function () {
+    var string = `Hello
+World!`;
+
+    expect(string).to.equal("Hello\n"+"World!");
+  
+  });
 });
