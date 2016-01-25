4242321547ef28bc31953c06c47e6173bc60a311
Mike Ulvila <mikeulvila@me.com>
2016-01-25 14:37:09 -0600

    first template string test

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -1,9 +1,10 @@
 var expect = require('chai').expect;
 
-describe('test suite', function () {
-  it('true should be true', function () {
-    expect(true).to.be.true;
-  
+describe('template strings', function () {
+  it('should be string', function () {
+    var string = `abc`;
+    expect(string).to.be.a('number');
+    
   });
 
 
